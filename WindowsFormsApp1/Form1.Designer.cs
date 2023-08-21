c005c58fa38dafab3ae09e82cecb0d86f952635e
Sasha <brdgn@bk.ru>
2023-08-21 13:05:23 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(54, 45);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(298, 30);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 124);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 214);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 314);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Курс (USD ЦБ РФ)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// пример "Курс" из книги Культина
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Добавить";
+            button1.Enabled = false;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // нажатие клавиши в поле редактирования
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+                return;
+
+            if (e.KeyChar >= ',')
+                e.KeyChar = ',';
+
+            if (e.KeyChar == ',')
+            {
+                /*
+                 в поле редактирования не м.б. больше одной запятой
+                 и запятая не м.б. первым символом
+                 */
+                if ((textBox1.Text.IndexOf(',') != -1) ||
+                    (textBox1.Text.Length ==  0))
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+
+            if (char.IsControl(e.KeyChar))
+            {
+                // Enter, Backspace, Escape
+                if (e.KeyChar == (char)Keys.Enter)
+                    // установить курсор на кнопку ОК
+                    button1.Focus();
+                return;
+            }
+            // остальные символы запрещены
+            e.Handled = true;
+        }
+
+        // изменилось содержимое поля редактирования
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;
+        }
+
+        // щелчок на кнопке "Добавить"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double kurs;   // курс
+            DateTime date; // дата
+
+            date = dateTimePicker1.Value;
+            kurs = System.Convert.ToDouble(textBox1.Text);
+
+            // Получить информацию о файле
+            System.IO.FileInfo fi =
+                new System.IO.FileInfo(
+                    Application.StartupPath + "\\usd.txt");
+
+            // поток для записи
+            System.IO.StreamWriter sw;
+
+            if (fi.Exists) // файл данных существует?
+                // откроем поток для добавления
+                sw = fi.AppendText();
+            else
+                // создать файл и открыть поток для записи
+                sw = fi.CreateText();
+
+            // запись в файл
+            sw.WriteLine(date.ToShortDateString());
+            sw.WriteLine(kurs.ToString("N"));
+
+            // закрыть поток
+            sw.Close();
+
+            // чтобы по ошибке не записать данные
+            // второй раз, сделаем недоступными
+            // поле ввода и кнопку
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+        }
+
+        // пользователь выбрал другую дату
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            // очистить поле и сделать доступным поле ввода
+            textBox1.Enabled = true;
+            textBox1.Clear();
+            // установить курсор в поле ввода
+            textBox1.Focus();
+        }
+    }
+}
